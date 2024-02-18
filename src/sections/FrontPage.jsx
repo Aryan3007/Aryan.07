32ef5ff3b7646b2d5af38dd53d8dcf1bdc0b53e3
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-02-18 15:13:49 +0530

    fixed front page light mode

--- src/sections/FrontPage.jsx
+++ src/sections/FrontPage.jsx
@@ -2,7 +2,7 @@
 const FrontPage = () => {
   return (
     <>
-      <section className="bg-gray-900 text-white">
+      <section className="bg-white dark:bg-gray-900 text-white">
         <div className="mx-auto max-w-screen-xl px-4 pt-44 lg:flex lg:h-full lg:items-center">
           <div className="mx-auto max-w-3xl text-center">
             <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
