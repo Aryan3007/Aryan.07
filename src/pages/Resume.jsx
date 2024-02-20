de89a00d1b8495968c6d44dc6e868f8cf8a12d26
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-02-20 17:43:30 +0530

    resume changes

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,73 +1,127 @@
-// import { useState } from "react"
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
 
 
 const Resume = () => {
-  // const [skills, setSkills]=useState([
-  //   {
-
-  //   }
-  // ])
+  const [skills, setSkills] = useState([
+    {
+      lang: "React JS",
+      level: "Advance",
+      logo: <FaReact />,
+      color: "text-[#64d5f4]",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "Next JS",
+      level: "Advance",
+      logo: <SiNextdotjs />,
+      color: "",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "Node JS",
+      level: "Intermediate",
+      logo: <FaNodeJs />,
+      color: "text-[#84c86e]",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "Express JS",
+      level: "Advance",
+      logo: <SiExpress />,
+      color: "",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "MongoDB",
+      level: "Advance",
+      logo: <FaHtml5 />,
+      color: "",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "Javascript",
+      level: "Advance",
+      logo: <IoLogoJavascript />,
+      color: "text-yellow-500",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "CSS",
+      level: "Advance",
+      logo: <FaHtml5 />,
+      color: "",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "HTML",
+      level: "Advance",
+      color: "",
+      logo: <FaHtml5 />,
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+    {
+      lang: "React Native",
+      level: "Beginner",
+      logo: <FaHtml5 />,
+      color: "text-red-500",
+      discription:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci distinctio ali voluptatum blanditiis laudantium.",
+    },
+  ]);
 
   return (
     <section className="dark:bg-gray-900 dark:text-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-    <div className="mx-auto max-w-lg text-center">
-      <h2 className="text-3xl font-bold sm:text-4xl">Kickstart your marketing</h2>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="mx-auto max-w-lg text-center">
+          <h2 className="text-3xl font-bold sm:text-4xl">
+            Kickstart your marketing
+          </h2>
 
-      <p className="mt-4 text-gray-300">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus
-        nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum.
-      </p>
-    </div>
+          <p className="mt-4 text-gray-300">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam
+            fugit consequuntur saepe laborum.
+          </p>
+        </div>
 
-    <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {skills.map((s, index) => (
+            <div
+              key={index}
+              className={`block rounded-xl border border-gray-800 p-8 shadow-xl hover:-translate-y-2 transition hover:border-blue-500/10 hover:shadow-blue-500/20 ${s.color}`}
+              href="#"
+            >
+              <div>
+                <span className="text-3xl">{s.logo}</span>
+              </div>
 
-    <div
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+              <h2 className="mt-4 text-xl font-bold text-white">
+                {s.lang}{" "}
+                <span className="font-semibold text-sm">{`(${s.level})`}</span>
+              </h2>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
-
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
+              <p className="mt-1 text-sm text-gray-300">{s.discription}</p>
+            </div>
+          ))}
+        </div>
       </div>
+    </section>
+  );
+};
 
-
-
-    </div>
-
-    <div className="mt-12 text-center">
-      <a
-        href="#"
-        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-      >
-        Get Started Today
-      </a>
-    </div>
-  </div>
-</section>
-  )
-}
-
-export default Resume
+export default Resume;
